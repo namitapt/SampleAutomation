2961f9f5a0a064e8d276101d2f49634bfc647b0b
namitapt <namita161098@gmail.com>
2022-09-05 16:13:52 +0530

    First commit.

---
+++ SampleAutomation/src/main/java/dev/TestDev.java
@@ -0,0 +1,10 @@
+package dev;
+
+
+public class TestDev {
+
+	
+	public void devTest() {
+		System.out.println("---Am in the Dev Test");
+	}
+}
---
+++ SampleAutomation/src/test/java/test/TestHelloWorld.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.junit.Test;
+
+public class TestHelloWorld {
+
+	@Test
+	public void secondTest() {
+		System.out.println("Hello am in the second test");
+	}
+}
